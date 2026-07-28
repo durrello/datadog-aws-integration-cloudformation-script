3070846b4e3dde2990fda08ef2e4a06f3be91e2f
Durrell Gemuh <durrellgemuh07@gmail.com>
2026-07-28 22:54:57 +0100

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # S3 Bucket Module
 module "s3_bucket" {
   source      = "./cloudformation/s3-bucket"
-  bucket_name = var.bucket_name  
+  bucket_name = var.bucket_name
 }
 
 # CloudFormation Module
 module "cloudformation" {
   source       = "./cloudformation"
-  stack_name   = var.stack_name 
+  stack_name   = var.stack_name
   template_url = module.s3_bucket.template_url
-  
-  depends_on = [module.s3_bucket] 
+
+  depends_on = [module.s3_bucket]
 }
